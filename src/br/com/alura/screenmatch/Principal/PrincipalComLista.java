17243fc8080ae264dec43038e0b1387938bb2dcf
jonasluis <jonasluis66@gmail.com>
2025-04-21 15:38:09 -0300

    refactor: ArrayList lista para List lista

--- src/br/com/alura/screenmatch/Principal/PrincipalComLista.java
+++ src/br/com/alura/screenmatch/Principal/PrincipalComLista.java
@@ -4,9 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComLista {
     public static void main(String[] args) {
@@ -18,7 +16,8 @@ public class PrincipalComLista {
         filmeJonas.avalia(8);
         Serie lucifer = new Serie("Lucifer", 2000);
         lucifer.avalia(10);
-        ArrayList<Titulo> lista = new ArrayList<>();
+
+        List<Titulo> lista = new LinkedList<>();
         lista.add(thor);
         lista.add(vingadores);
         lista.add(filmeJonas);
