30d60751e74d427551a29959447e64c4d03fbb3e
jonasluis <jonasluis66@gmail.com>
2025-04-24 05:44:23 -0300

    feat: service para consumir dados

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,5 +15,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Primeiro projeto Spring sem web");
+
+		ConsumoApi consumoApi = new ConsumoApi();
+		String json = consumoApi.obterDados("http://www.omdbapi.com/?t=divertidamente&apikey=777ff11c");
+		System.out.println(json);
 	}
 }
