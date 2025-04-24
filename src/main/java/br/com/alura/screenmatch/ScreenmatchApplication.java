af879a41ad686f6e9fd3d6b6de16af3fe33a8aa9
jonasluis <jonasluis66@gmail.com>
2025-04-24 06:06:29 -0300

    feat: Modelando dados da serie

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +19,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		System.out.println("Primeiro projeto Spring sem web");
 
 		ConsumoApi consumoApi = new ConsumoApi();
-		String json = consumoApi.obterDados("http://www.omdbapi.com/?t=divertidamente&apikey=777ff11c");
+		String json = consumoApi.obterDados("http://www.omdbapi.com/?t=lucifer&apikey=777ff11c");
 		System.out.println(json);
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+
 	}
 }
