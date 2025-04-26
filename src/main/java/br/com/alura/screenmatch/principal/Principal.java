f0a5d2b86d8774884d3af9042dfd853af9d0db77
jonasluis <jonasluis66@gmail.com>
2025-04-26 11:20:42 -0300

    feat: Buscar episódios a partir de uma data

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,6 +7,8 @@ import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -73,6 +75,21 @@ public class Principal {
                 .collect(Collectors.toList());              // Coleta tudo numa lista final de Episodio
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano voce deseja ver os épisodios");
+        var ano = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episodio: " + e.getTitulo() +
+                                " Data de Lançamento: " +  e.getDataLancamento().format(formatador)
+                ));
+
 
     }
 
