8c2ddf562ca5bd67b42036889f6d2237423dbcfe
jonasluis <jonasluis66@gmail.com>
2025-04-26 08:53:23 -0300

    feat: Modelando dados das temporadas

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.DadoEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -28,6 +32,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=lucifer&season=1&episode=2&apikey=777ff11c");
 		DadoEpisodio dadoEpisodio = converteDados.obterDados(json, DadoEpisodio.class);
 		System.out.println(dadoEpisodio);
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i < dadosSerie.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=lucifer&season=" + i +"&apikey=777ff11c");
+			DadosTemporada dadosTemporada = converteDados.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 
 	}
 }
