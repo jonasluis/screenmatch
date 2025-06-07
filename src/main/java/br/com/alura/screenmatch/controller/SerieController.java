cf31355ab631d3482ff0f59f5d045d42367a9e21
jonasluis <jonasluis66@gmail.com>
2025-06-07 10:13:03 -0300

    feat: Criar service para buscar todas as series

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,35 +1,23 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/series")
 public class SerieController {
   @Autowired
-  private SerieRepository repository;
+  private SerieService serieService;
 
   @GetMapping()
-  public List<SerieDTO> obterSeries(){
-    return repository.findAll()
-            .stream()
-            .map(s -> new SerieDTO(
-                    s.getId(),
-                    s.getTitulo(),
-                    s.getTotalTemporadas(),
-                    s.getAvaliacao(),
-                    s.getGenero(),
-                    s.getAtores(),
-                    s.getPoster(),
-                    s.getSinopse())).collect(Collectors.toList());
+  public List<SerieDTO> obterSeries() {
+    return serieService.obterTodasAsSeries();
   }
-
 }
